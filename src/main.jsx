606a95c9c87b2643f1ec4dcbcfbecf2d751d10d0
Dharmendra Chauhan <dmma359800@gmail.com>
2025-12-25 23:32:25 +0530

    add code

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import './index.css'
 import ReactDOM from 'react-dom/client';
-import { Route, RouterProvider, createRoutesFromElements,createHashRouter } from 'react-router-dom';
+import {
+  Route,
+  RouterProvider,
+  createRoutesFromElements,
+  createBrowserRouter
+} from 'react-router-dom';
+
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import Contact from './components/Contact/Contact.jsx'
@@ -10,22 +16,21 @@ import About from './components/About/About.jsx'
 import Project from './components/Project/Project.jsx'
 import Skill from './components/Skill/Skill.jsx';
 
-const router = createHashRouter(
+const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="about" element={<About />} />
-      <Route path="contact" element={<Contact />}/>
-      <Route path="education" element={<Education />}/>
-      <Route path="project" element={<Project />}/>
-      <Route path="skill" element={<Skill />}/>
-    </ Route>
+      <Route path="contact" element={<Contact />} />
+      <Route path="education" element={<Education />} />
+      <Route path="project" element={<Project />} />
+      <Route path="skill" element={<Skill />} />
+    </Route>
   )
 )
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
