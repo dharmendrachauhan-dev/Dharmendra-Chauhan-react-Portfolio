07978512c4ba8065fae5dcd7d559e7d67c059c48
Dharmendra Chauhan <dmma359800@gmail.com>
2025-12-25 23:14:40 +0530

    fix github pages routing using hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css'
 import ReactDOM from 'react-dom/client';
-import { Route, RouterProvider, createRoutesFromElements, createBrowserRouter } from 'react-router-dom';
+import { Route, RouterProvider, createRoutesFromElements,createHashRouter } from 'react-router-dom';
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import Contact from './components/Contact/Contact.jsx'
@@ -10,10 +10,10 @@ import About from './components/About/About.jsx'
 import Project from './components/Project/Project.jsx'
 import Skill from './components/Skill/Skill.jsx';
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route path="/" element={<Home />} />
+      <Route index element={<Home />} />
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />}/>
       <Route path="education" element={<Education />}/>
